a90220af8d0d77bf09b9da648b84592f64e7f8b6
nimaaqakhani <nimaaqakhani2@gmail.com>
2026-06-01 05:42:53 +0330

    chore(ui): hide debug mode banner from MaterialApp
    - Set `debugShowCheckedModeBanner` to false in `main.dart`
    - Clean up the top-right screen corner for a cleaner development view

--- lib/main.dart
+++ lib/main.dart
@@ -16,6 +16,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Bluetooth Temperature',
       theme: ThemeData(primarySwatch: Colors.blue),
       home: BlocProvider(
